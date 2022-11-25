01808b9e5c18b82b3cb0f4d6a851af1b21c3ff34
Korneechev Evgeniy <ekorneechev@gmail.com>
2022-11-25 17:31:44 +0300

    New bug-fix release - 2.4.1

--- lib/config.py
+++ lib/config.py
@@ -38,7 +38,7 @@ else:
             os.system( "zenity --error --icon-name=myconnector --text=\"%s\"" % self.text )
 
 APP         = "myconnector"
-VERSION     = "2.4"
+VERSION     = "2.4.1"
 
 _global_conf_file = "/etc/%s/%s.conf" % ( APP, APP )
 
